763746ef39d7d35248ecc13d6216f7474621f3c0
matej <mmuller@student.unipu.hr>
2022-10-10 19:45:14 +0200

    Rijesen zadatak 3

---
+++ vjezbe 01/zadatak3.js
@@ -0,0 +1,8 @@
+function funkcija (x) {
+    if (x >= 100 && x <= 150000){
+        console.log(x + " je unutar [100, 150000]")
+    }
+    else console.log(x + " nije unutar [100, 150000]")
+}
+
+funkcija(99)
